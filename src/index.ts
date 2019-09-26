fb1997212f42bb9f787dab120604e187428bc322
Dmitry Zhukov <zhukov.dima.40@gmail.com>
2019-09-26 17:44:36 +0300

    routing-controllers was connected

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+import "reflect-metadata";
 import { App } from './app';
+import { DataBase } from './db'
+import { config } from './ormconfig';
+
+import { useExpressServer } from 'routing-controllers';
 
 async function main() {
-    const app = new App();
+
+    //await DataBase.connection(config);
+    const app = new App(); //new App(<PORT>);
+    console.log(__dirname + '/controllers/*.*')
+    useExpressServer(app.app, {
+        routePrefix: '/api',
+        controllers: [__dirname + '/controllers/*'],
+    });
     await app.listen();
 }
 
