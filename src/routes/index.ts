d9c7f1d89513cc22dfdb6eb437062090419b1e31
Dmitry Zhukov <zhukov.dima.40@gmail.com>
2019-09-26 17:45:44 +0300

    routes folder was deleted

--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { indexMain } from '../controllers/index.controller';
-
-const router = Router();
-
-router.route('/').get(indexMain);
-
-export default router;
