dacdcbfd130b671daba9bb4b770634e6dd2e0650
Dmitry Zhukov <zhukov.dima.40@gmail.com>
2019-09-26 17:42:56 +0300

    routers was removed

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,13 @@
 import express, { Application } from 'express';
 import logger from 'morgan';
 
-// Routes
-import IndexRoutes from './routes/index';
-
 export class App {
-    private app: Application;
+    public app: Application;
 
     public constructor(private port?: number) {
         this.app = express();
         this.settings();
         this.middlewares();
-        this.routes();
     }
 
     private settings() {
@@ -23,13 +19,13 @@ export class App {
         this.app.use(express.json());
     }
 
-    private routes() {
-        this.app.use(IndexRoutes);
-    }
-
     public async listen(): Promise<void> {
-        const PORT = this.app.get('port');
-        await this.app.listen(PORT);
-        console.log(`Server is running on port ${PORT}`);
+        try {
+            const PORT = this.app.get('port');
+            await this.app.listen(PORT);
+            console.log(`Server is running on port ${PORT}`);
+        } catch (error) {
+            console.log(`listen(): ${error}`)
+        }
     }
 }
